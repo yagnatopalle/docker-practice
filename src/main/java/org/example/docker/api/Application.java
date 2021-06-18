681ec8e16f06b66e6357245b1948ba73ec2fdcf9
Yagna Topalle <tynarayana@outlook.com>
2021-06-18 19:03:27 +1000

    added gradle support along with basic stubs

---
+++ src/main/java/org/example/docker/api/Application.java
@@ -0,0 +1,10 @@
+package org.example.docker.api;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
---
+++ src/main/java/org/example/docker/api/Greeter.java
@@ -0,0 +1,8 @@
+package org.example.docker.api;
+
+public class Greeter {
+
+    public String sayHello(){
+        return "Hello World!";
+    }
+}
